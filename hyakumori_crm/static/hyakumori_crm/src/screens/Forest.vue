fe0937080dd31f350169b7abc63435c961525dab
BaoHuynh <baohp@hybrid-technologies.vn>
2020-04-06 08:40:49 +0700

    search validation

--- hyakumori_crm/static/hyakumori_crm/src/screens/Forest.vue
+++ hyakumori_crm/static/hyakumori_crm/src/screens/Forest.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row class="px-7 pt-2">
     <v-col cols="3">
-      <search-card />
+      <search-card
+        :searchCriteria="getSearchCriteria"
+        @onSearch="onSearch"
+        @unableDelete="unableDelErr"
+        @conditionOutOfBounds="conditionOutOfBoundsErr"
+      />
     </v-col>
 
     <v-col cols="9">
@@ -15,9 +20,18 @@
         :data="getData"
         :showSelect="true"
         :negotiationCols="['status']"
+        :isLoading="isLoading"
         :serverItemsLength="getTotalForests"
       ></data-list>
     </v-col>
+
+    <snack-bar
+      color="error"
+      :isShow="isShowErr"
+      :msg="errMsg"
+      :timeout="sbTimeout"
+      @dismiss="onDismissSb"
+    />
   </v-row>
 </template>
 
@@ -27,14 +41,30 @@ import SearchCard from "../components/SearchCard";
 import TableAction from "../components/TableAction";
 import headers from "../assets/dump/table_header_forest.json";
 import GetForestList from "../graphql/GetForestList.gql";
+import SnackBar from "../components/SnackBar";
+import ScreenMixin from "./ScreenMixin";
 
 export default {
   name: "forest",
+
+  mixins: [ScreenMixin],
 
   components: {
     DataList,
     SearchCard,
     TableAction,
+    SnackBar,
+  },
+
+  data() {
+    return {
+      pageIcon: this.$t("icon.forest_icon"),
+      pageHeader: this.$t("page_header.forest_list"),
+      searchCriteria: [],
+      isShowErr: false,
+      errMsg: null,
+      sbTimeout: 5000,
+    };
   },
 
   apollo: {
@@ -48,11 +78,42 @@ export default {
     rowData() {
       // console.log(val);
     },
+
+    onSearch(err, data) {
+      if (err) {
+        this.isShowErr = true;
+        this.errMsg = this.$t("search.duplicate_criteria_msg");
+      } else {
+        // Handling search data
+      }
+    },
+
+    onDismissSb(val) {
+      this.isShowErr = val;
+    },
+
+    unableDelErr(err) {
+      if (err) {
+        this.isShowErr = true;
+        this.errMsg = this.$t("search.unable_to_remove_search_msg");
+      }
+    },
+
+    conditionOutOfBoundsErr(err) {
+      if (err) {
+        this.isShowErr = true;
+        this.errMsg = this.$t("search.condition_is_maximum");
+      }
+    },
   },
 
   computed: {
     getHeaders() {
       return headers;
+    },
+
+    isLoading() {
+      return this.$apollo.queries.forestsInfo.loading;
     },
 
     getData() {
@@ -79,6 +140,10 @@ export default {
       } else {
         return 0;
       }
+    },
+
+    getSearchCriteria() {
+      return Array.from(this.getHeaders).map(header => header.text);
     },
   },
 };
