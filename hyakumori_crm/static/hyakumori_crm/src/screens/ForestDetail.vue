317de8a79e3fa19453952d173bb827b3b2e24b4d
Nguyen Anh Tu <tuna@hybrid-technologies.co.jp>
2020-04-17 14:29:16 +0700

    login screen and ui fixes

--- hyakumori_crm/static/hyakumori_crm/src/screens/ForestDetail.vue
+++ hyakumori_crm/static/hyakumori_crm/src/screens/ForestDetail.vue
@@ -11,7 +11,7 @@
         <div class="my-4">
           <basic-info :infos="getBasicInfo" :isUpdate="isUpdate.basicInfo" />
           <update-button
-            class="mt-n3 mb-12"
+            class="mb-12"
             v-if="isUpdate.basicInfo"
             :cancel="cancel.bind(this, 'basicInfo')"
           />
@@ -66,7 +66,7 @@
           :cancel="cancel.bind(this, 'discussion')"
         />
         <p class="forest-detail__expand" @click="expandDiscussionList">
-          {{ isExpand ? '一部表示する' : 'すべて表示する' }}
+          {{ isExpand ? "一部表示する" : "すべて表示する" }}
         </p>
 
         <content-header
@@ -164,7 +164,7 @@ export default {
       forestInfo: null,
       forestOwners: null,
       pageIcon: this.$t("icon.forest_icon"),
-      backBtnContent: this.$t("page_header.forest_list"),
+      backBtnContent: this.$t("page_header.forest_mgmt"),
       headerTagColor: "#FFC83B",
       isExpand: false,
       isUpdate: {
