7a5f7331475e5abf31985f76c872933208278ad9
Nguyen Anh Tu <tuna@hybrid-technologies.co.jp>
2020-04-22 16:47:47 +0700

    refactor page header

--- hyakumori_crm/static/hyakumori_crm/src/components/MainSection.vue
+++ hyakumori_crm/static/hyakumori_crm/src/components/MainSection.vue
@@ -1,13 +1,27 @@
 <template>
-  <v-container fluid class="app-width d-flex px-7 pt-5">
-    <slot name="section"></slot>
-    <slot name="right"></slot>
-  </v-container>
+  <div>
+    <slot name="top">
+      <page-header></page-header>
+    </slot>
+
+    <v-container fluid class="app-width d-flex px-7 pt-5" v-if="!singleColumn">
+      <slot name="section"></slot>
+      <slot name="right"></slot>
+    </v-container>
+
+    <slot name="center"></slot>
+  </div>
 </template>
 
 <script>
+import PageHeader from "./PageHeader";
+
 export default {
   name: "main-section",
+  components: { PageHeader },
+  props: {
+    singleColumn: { type: Boolean, default: false },
+  },
 };
 </script>
 
