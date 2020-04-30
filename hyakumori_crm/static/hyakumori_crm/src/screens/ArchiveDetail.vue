253bb75755903ac4830aab8aa29faa9e3b8b6d38
BaoHuynh <baohp@hybrid-technologies.vn>
2020-04-30 16:19:45 +0700

    create archive, display & edit basic info

--- hyakumori_crm/static/hyakumori_crm/src/screens/ArchiveDetail.vue
+++ hyakumori_crm/static/hyakumori_crm/src/screens/ArchiveDetail.vue
@@ -106,7 +106,14 @@ export default {
       participants: [
         {
           customer_id: "123",
-          fullname: "山田太郎",
+          name_kanji: {
+            last_name: "石原",
+            first_name: "正",
+          },
+          name_kana: {
+            last_name: "イシハラ",
+            first_name: "タダシ",
+          },
           address: "424-0023 岡山県倉敷市大谷4-1-3",
           email: "hanako.yamada@gmail.com",
           forest_count: 4,
@@ -115,7 +122,14 @@ export default {
         },
         {
           customer_id: "123",
-          fullname: "山田太郎",
+          name_kanji: {
+            last_name: "石原",
+            first_name: "正",
+          },
+          name_kana: {
+            last_name: "イシハラ",
+            first_name: "タダシ",
+          },
           address: "424-0023 岡山県倉敷市大谷4-1-3",
           email: "hanako.yamada@gmail.com",
           forest_count: 4,
