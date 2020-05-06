1088dabc8bbfac16406bc28c2d29f79126238800
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-05-06 17:56:17 +0700

    init

--- hyakumori_crm/static/hyakumori_crm/src/components/detail/ArchiveParticipantContainer.vue
+++ hyakumori_crm/static/hyakumori_crm/src/components/detail/ArchiveParticipantContainer.vue
@@ -7,11 +7,51 @@
       :loading="isLoading"
       @toggleEdit="val => (isUpdate = val)"
     />
-    <slot name="participants" :isUpdate="isUpdate"></slot>
-    <template v-if="isUpdate">
-      <addition-button class="my-2" :content="addBtnContent" />
-      <update-button :cancel="cancel.bind(this)" />
-    </template>
+    <customer-contact-list :contacts="participants" :isUpdate="isUpdate" />
+    <addition-button
+      ref="addBtn"
+      class="my-2"
+      v-if="isUpdate"
+      :content="addBtnContent"
+      :click="() => (showSelect = true)"
+    />
+    <select-list-modal
+      :loading="loadContacts"
+      :shown.sync="showSelect"
+      :submitBtnText="$t('buttons.add')"
+      submitBtnIcon="mdi-plus"
+      :handleSubmitClick="handleAdd"
+      @needToLoad="handleLoadMore"
+      @search="debounceLoadInitContactsForAdding"
+    >
+      <template #list>
+        <customer-contact-card
+          @click="
+            (cId, inx) => {
+              modalSelectingContactId = cId;
+              modalSelectingContactIndex = inx;
+            }
+          "
+          v-for="(item, indx) in participantsForAdding"
+          :key="item.id"
+          :card_id="item.id"
+          :contact="item"
+          :showAction="false"
+          :index="indx"
+          :selectedId="modalSelectingContactId"
+          flat
+          mode="search"
+          :showRelationshipSelect="false"
+        />
+      </template>
+    </select-list-modal>
+    <update-button
+      v-if="isUpdate"
+      :cancel="() => (isUpdate = !isUpdate)"
+      :save="handleSave"
+      :saving="saving"
+      :saveDisabled="saveDisabled"
+    />
   </div>
 </template>
 
@@ -19,7 +59,11 @@
 import ContentHeader from "./ContentHeader";
 import ContainerMixin from "./ContainerMixin";
 import UpdateButton from "./UpdateButton";
+import CustomerContactList from "./CustomerContactList";
+import CustomerContactCard from "./CustomerContactCard";
 import AdditionButton from "../AdditionButton";
+import SelectListModal from "../SelectListModal";
+import { debounce } from "lodash";
 
 export default {
   name: "archive-participant-container",
@@ -30,12 +74,68 @@ export default {
     ContentHeader,
     UpdateButton,
     AdditionButton,
+    SelectListModal,
+    CustomerContactList,
+    CustomerContactCard,
   },
-
+  props: {
+    participants: { type: Array, default: () => [] },
+  },
+  created() {
+    this.debounceLoadInitContactsForAdding = debounce(
+      this.loadInitContacts,
+      500,
+    );
+  },
   data() {
     return {
       isUpdate: false,
+      loadContacts: false,
+      contactsForAdding: {},
+      saving: false,
+      modalSelectingContactId: null,
+      modalSelectingContactInde: null,
+      showSelect: false,
     };
+  },
+  computed: {
+    saveDisabled() {
+      return true;
+    },
+    participantsForAdding() {
+      return this.contactsForAdding.results || [];
+    },
+  },
+  methods: {
+    handleSave() {},
+    handleAdd() {},
+    async handleLoadMore() {
+      if (!this.contactsForAdding.next && this.loadContacts) return;
+      this.loadContacts = true;
+      const resp = await this.$rest.get(this.contactsForAdding.next);
+      this.contactsForAdding = {
+        next: resp.next,
+        previous: resp.previous,
+        results: [...this.contactsForAdding.results, ...resp.results],
+      };
+      this.loadContacts = false;
+    },
+    async loadInitContacts(keyword) {
+      this.loadContacts = true;
+      this.contactsForAdding = await this.$rest.get("/contacts", {
+        params: {
+          search: keyword || "",
+        },
+      });
+      this.loadContacts = false;
+    },
+  },
+  watch: {
+    async showSelect(val) {
+      if (val && !this.contactsForAdding.next) {
+        await this.loadInitContacts();
+      }
+    },
   },
 };
 </script>
