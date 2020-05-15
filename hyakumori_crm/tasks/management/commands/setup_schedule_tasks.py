07d31fe72fe40a682bc2eb1f64b4397ff1fda977
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-05-15 11:45:58 +0700

    wrap schedule func to avoid duplicate

--- hyakumori_crm/tasks/management/commands/setup_schedule_tasks.py
+++ hyakumori_crm/tasks/management/commands/setup_schedule_tasks.py
@@ -3,19 +3,21 @@ from distutils.util import strtobool
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django_q.models import Schedule
-from django_q.tasks import schedule
 
 from hyakumori_crm.permissions.services import PermissionService
 from hyakumori_crm.activity.services import ActivityService
 from hyakumori_crm.tags.services import TagService
+
+from ...schedule import create_schedule
 
 
 class Command(BaseCommand):
     help = "Setup schedule tasks"
 
     def handle(self, *args, **kwargs):
-        schedule(
-            "hyakumori_crm.tasks.healthcheck.do_healthcheck",
+        create_schedule(
+            func="hyakumori_crm.tasks.healthcheck.do_healthcheck",
+            name="do_healthcheck",
             schedule_type=Schedule.MINUTES,
             minutes=1,
         )
