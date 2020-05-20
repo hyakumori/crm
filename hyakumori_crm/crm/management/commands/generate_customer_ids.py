370ed7b065e0c7ce9d923ad101cce7ce577a75f1
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-05-20 15:56:24 +0700

    fix command using transaction atomic

--- hyakumori_crm/crm/management/commands/generate_customer_ids.py
+++ hyakumori_crm/crm/management/commands/generate_customer_ids.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from sequences.models import Sequence
 
 from hyakumori_crm.crm.common.constants import CUSTOMER_ID_PREFIX, CUSTOMER_ID_SEQUENCE
@@ -26,17 +27,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         recreate = kwargs.get("recreate")
-        if recreate:
-            Sequence.objects.filter(name=CUSTOMER_ID_SEQUENCE).update(last=0)
         qs = Customer.objects.order_by(
             "name_kanji__last_name", "name_kanji__first_name"
         )
-        for customer in qs.iterator():
-            if not customer.business_id or len(customer.business_id) == 0 or recreate:
-                customer.business_id = generate_sequential_id(
-                    CUSTOMER_ID_PREFIX, CUSTOMER_ID_SEQUENCE
-                )
-                customer.save()
-                self.stdout.write(customer.business_id)
+        with transaction.atomic():
+            if recreate:
+                Sequence.objects.filter(name=CUSTOMER_ID_SEQUENCE).update(last=0)
+            for customer in qs.iterator():
+                if (
+                    not customer.business_id
+                    or len(customer.business_id) == 0
+                    or recreate
+                ):
+                    customer.business_id = generate_sequential_id(
+                        CUSTOMER_ID_PREFIX, CUSTOMER_ID_SEQUENCE
+                    )
+                    customer.save(update_fields=["business_id"])
+                    self.stdout.write(customer.business_id)
 
         self.stdout.write("DONE")
