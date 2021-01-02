4bb4aa5d932ebf5222e81ec3eca00cfa6972f4bc
Nathaniel <nihongotraveler@icloud.com>
2021-01-02 16:53:16 +0900

    [map] Added MapContainer.vue with basic building blocks.
    Signed-off-by: Torgian <nathaniel@georepublic.de>

---
+++ hyakumori_crm/static/hyakumori_crm/src/components/MapContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <content-header
+      :content="headerContent"
+    />
+  </div>
+</template>
+<script>
+import ContainerMixin from "./ContainerMixin";
+import ContentHeader from "./ContentHeader";
+
+export default {
+  name: "map-container",
+
+  mixins: [ContainerMixin, SelectListModalMixin],
+
+  components: {
+    ContentHeader,
+  }
+}
+</script>
